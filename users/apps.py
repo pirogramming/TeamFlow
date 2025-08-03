1ce880ac3828aa1093b6b1983e475d4e4cfe3dc2
subeeen <rose9165676@naver.com>
2025-08-03 16:19:19 +0900

    feat: 랜딩페이지와 구글 로그인 연결 완료

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,29 @@
+# users/apps.py
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        from allauth.socialaccount.models import SocialApp
+        from django.contrib.sites.models import Site
+        import os
+
+        try:
+            site = Site.objects.get_current()
+
+            client_id = os.getenv("GOOGLE_CLIENT_ID")
+            secret = os.getenv("GOOGLE_SECRET")
+
+            if not SocialApp.objects.filter(provider="google").exists():
+                app = SocialApp.objects.create(
+                    provider="google",
+                    name="Google",
+                    client_id=client_id,
+                    secret=secret,
+                )
+                app.sites.add(site)
+        except:
+            pass
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import GoogleLoginAPIView, login_page
+from .views import landing_page
 
 urlpatterns = [
-    path('login/', login_page, name='login-page'),
-    path('google/', GoogleLoginAPIView.as_view(), name='google-login'),
+    path('', landing_page, name='landing_index'),
 ]
