7923498c7004a2549547de18aee5e58fcce189b5
subeeen <rose9165676@naver.com>
2025-08-12 22:12:02 +0900

    fix: 웹소켓을 위한 CHANNEL_LAYERS 수정, .env 파일 수정

--- users/adapters.py
+++ users/adapters.py
@@ -40,8 +40,6 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         else:
             # 새 사용자 또는 프로필 미완성 사용자: 프로필 설정 페이지로
             return reverse('profile-setup')
-        
-        return '/'
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     """
